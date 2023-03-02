5b80fd2015e12708e3fd96b8722e347fcf5f84d8
Nikolay Nikolov <3ndien@gmail.com>
2023-03-02 04:32:53 +0200

    add room.model.ts, config/default.ts, logger.ts,

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello Node TS');
+import express from 'express';
+import { connect } from './utils/dbConnect';
+import config from 'config';
+import logger from './utils/logger';
+import routes from './router';
+
+const app = express();
+const port = config.get<number>('port');
+
+app.use(express.json());
+
+app.listen(port, async () => {
+  logger.info(`app is running on http://localhost:${port}`);
+
+  await connect();
+  routes(app);
+});
